777d4e370ff14e2cc22b0083cdaa62671f8e16f5
Fazar Budiman <fazarbudiman1907@gmail.com>
2023-11-26 15:40:40 +0700

    change-style-drawer-menu

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,8 +1,8 @@
-import { Button, Col, Drawer, Grid, Menu, Row } from "antd";
+import { Button, Col, Drawer, Flex, Grid, Menu, Row } from "antd";
 import { Header } from "antd/es/layout/layout";
 import logoNitroboost from "../assets/images/logo-nitroboost.png";
 import { useState } from "react";
-import { FaBars } from "react-icons/fa";
+import { LuMenu } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
 const { useBreakpoint } = Grid;
 import "../assets/styles/header.css";
@@ -59,7 +59,7 @@ const Headers = () => {
         </Col>
         <Col xs={4} sm={12} md={6} lg={6} xl={4}>
           {(xs && !md) || (sm && !md) || (!xs && !sm && !md) ? (
-            <FaBars className="icon-hamburger-menu" size={22} onClick={showDrawer} />
+            <LuMenu className="icon-hamburger-menu" size={22} onClick={showDrawer} />
           ) : (
             <Menu className="list-menu" mode="horizontal" onClick={(e) => onClickMenu(e.key)} items={itemsMenu} />
           )}
@@ -67,7 +67,6 @@ const Headers = () => {
       </Row>
 
       <Drawer
-        className="drawer-menu"
         styles={{
           header: {
             border: "0px",
@@ -81,40 +80,44 @@ const Headers = () => {
         closable={false}
         open={open}
       >
-        <Row gutter={[0, 20]}>
-          <Col span={24} className="drawer-menu-close">
-            <IoClose size={26} onClick={onClose} />
-          </Col>
-          <Col span={24} className="drawer-menu-logo">
-            <img src={logoNitroboost} />
-          </Col>
-          <Col span={24} className="drawer-menu">
-            <Link to="/tentang-kami">Tentang Kami</Link>
-          </Col>
-          <Col span={24} className="drawer-menu">
-            <Link to="/fasilitas">Fasilitas</Link>
-          </Col>
-          <Col span={24} className="drawer-menu">
-            <Link to="/galeri">Galeri</Link>
-          </Col>
-          <Col span={20} offset={2}>
-            <Link to="/join-us">
-              <Button className="button-menu-item" type="primary" size="middle">
-                Daftar
-              </Button>
-            </Link>
-          </Col>
-          <Col span={24} className="disclaimer-card">
-            <p>
-              <strong>Disclaimer</strong>
-              <br />
+        <Flex vertical align="center" justify="space-between" className="drawer-content">
+          <Row gutter={[0, 20]} className="drawer-content-menu">
+            <Col xs={{ span: 24 }} sm={{ offset: 1, span: 22 }} className="drawer-menu-close">
+              <IoClose size={26} onClick={onClose} />
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ offset: 1, span: 22 }} className="drawer-menu-logo">
+              <img src={logoNitroboost} />
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ offset: 1, span: 22 }} className="drawer-menu">
+              <Link to="/tentang-kami">Tentang Kami</Link>
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ offset: 1, span: 22 }} className="drawer-menu">
+              <Link to="/fasilitas">Fasilitas</Link>
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ offset: 1, span: 22 }} className="drawer-menu">
+              <Link to="/galeri">Galeri</Link>
+            </Col>
+            <Col span={20} offset={2}>
+              <Link to="/join-us">
+                <Button className="button-menu-item" type="primary" size="middle">
+                  Daftar
+                </Button>
+              </Link>
+            </Col>
+          </Row>
+          <Row className="drawer-content-disclaimer">
+            <Col xs={{ span: 24 }} sm={{ offset: 1, span: 22 }} className="disclaimer-card">
               <p>
-                Seberapapun hasil portofolio orang lain atau sekuat apapun trading seseorang yang diikuti. Trading itu sendiri, kembali kepada pemahaman pemain/user terhadap manajemen resiko dan manajemen keuangan yang baik, serta
-                psikologis trading dalam menentukan rencana trading itu sendiri.
+                <strong>Disclaimer</strong>
+                <br />
+                <p>
+                  Seberapapun hasil portofolio orang lain atau sekuat apapun trading seseorang yang diikuti. Trading itu sendiri, kembali kepada pemahaman pemain/user terhadap manajemen resiko dan manajemen keuangan yang baik, serta
+                  psikologis trading dalam menentukan rencana trading itu sendiri.
+                </p>
               </p>
-            </p>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </Flex>
       </Drawer>
     </Header>
   );
